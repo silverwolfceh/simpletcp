50311087b119187c222a3da935a9351ec0953add
SilverWolfCEH <silverwolf@ceh.vn>
2018-11-01 14:02:08 +0700

    First draft

--- server.py
+++ server.py
@@ -1,16 +1,41 @@
-def start_server():
+from flask import Flask
+from flask_cors import CORS
+from threading import Thread
 
-    server_config = config['Server']
+client_conn = None
+app = Flask(__name__)
+CORS(app)
 
-    # configurations
-    HOST = server_config['HOST']
-    PORT = int(environ.get('PORT')) #server_config.getint('PORT')
-    BUFFER_SIZE = server_config.getint('BUFFER_SIZE')
+@app.route('/send/<msg>')
+def send_mesage_to_client(msg):
+    print("Sending: %s" % msg)
+    if client_conn != None:
+        client_conn.send(msg)
+        return "Sent"
+    else:
+        return "Client not connect"
+
+def socket_srv():
+    global client_conn
+    HOST = "127.0.0.1"
+    PORT = "12345"
 
     # server socket setup
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((HOST, PORT))
-    server_socket.listen(server_config.getint('LIMIT'))
+    server_socket.listen(1) # One backlog connection
+    while True:
+        client_conn, addr = s.accept()
+        print("Connected with " + addr[0] + ":" + str(addr[1]))
+    return server_socket
 
-    logging.info('server started %s:%d', HOST, PORT)
+def thread_srv(arg):
+	socket_srv()
+	print("Thread srv end!")
+
+if __name__ == '__main__':
+    t = Thread(target = thread_srv, args = (" ", ))
+    t.start()
+    app.run()
+
