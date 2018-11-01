475ad6f333beb2ba389a5c31ac9f30f0c17a9910
SilverWolfCEH <silverwolf@ceh.vn>
2018-11-01 14:38:05 +0700

    Switch back to gunicorn

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
 import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('localhost', 12345)
+server_address = ('updatesrv.herokuapp.com', 12345)
 sock.connect(server_address)
 while True:
     data = sock.recv(1024)
